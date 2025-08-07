d306ca97a939708a04c494af775fe2ecda55f61e
peperoncinopasuta <peperoncino.pasuta@gmail.com>
2025-08-07 19:23:02 +0900

    いったんタイトルつくった

---
+++ Assets/titele_FurikoDragScript.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class titele_FurikoDragScript : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private Rigidbody2D endPointRigidbody;
+
+    private bool isDragging = false;//掴んでいるか
+    private Vector2 dragOffset;
+
+    public bool isPull=false;//引っ張ったか
+    public float pullBorder_Y;//引っ張り判定の境界ライン値
+    public int pullCnt = 0;//引っ張った回数
+
+
+    void Update()
+    {
+        // マウス左クリック
+        if (Input.GetMouseButtonDown(0))
+        {
+            //マウスの画面座標をワールド座標に変換
+            Vector2 mouseWorldPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+
+            //マウスの位置に当たり判定があるか
+            Collider2D hit = Physics2D.OverlapPoint(mouseWorldPos);
+
+            //掴んだオブジェクトが終点なら
+            if (hit != null && hit.attachedRigidbody == endPointRigidbody)
+            {
+                isDragging = true;
+                dragOffset = endPointRigidbody.position - mouseWorldPos;
+
+                // ドラッグ中は物理停止
+                endPointRigidbody.isKinematic = true;
+            }
+        }
+
+        // マウス離したとき
+        if (Input.GetMouseButtonUp(0) && isDragging)
+        {
+            isDragging = false;
+
+            // 物理挙動再開
+            endPointRigidbody.isKinematic = false;
+        }
+
+        // ドラッグ中は終点をマウスについてくる
+        if (isDragging)
+        {
+            Vector2 mouseWorldPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 targetPos = mouseWorldPos + dragOffset;
+
+            endPointRigidbody.MovePosition(targetPos);
+        }
+
+
+        //ボーダーまで引っ張ったら
+        if(endPointRigidbody.position.y <pullBorder_Y)
+        {
+
+            isPull = true;
+            pullCnt++;
+
+        }
+
+
+
+    }
+}
